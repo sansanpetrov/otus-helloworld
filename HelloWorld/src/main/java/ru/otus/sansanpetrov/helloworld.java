21276f2d1d616229230fd2f284331e2ebba8ab0b
Alexander Petrov <san.san.petrov@yandex.ru>
2023-07-10 09:37:56 +0300

    Initial commit

---
+++ HelloWorld/src/main/java/ru/otus/sansanpetrov/helloworld.java
@@ -0,0 +1,7 @@
+package ru.otus.sansanpetrov;
+// Создаем класс helloworld
+public class helloworld {
+    public static void main(String args []) { // Создаем метод main (основной метод, тело программы)
+System.out.println("Hello, World!"); // Выводим строку в консоль
+    }
+}
