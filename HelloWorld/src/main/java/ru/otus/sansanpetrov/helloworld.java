c72df65e52a625c018ed074ec70c8cdad9aa17a8
sansanpetrov <san.san.petrov@yandex.ru>
2023-07-11 10:33:33 +0300

    Первые три параграфа ДЗ 1

--- HelloWorld/src/main/java/ru/otus/sansanpetrov/helloworld.java
+++ HelloWorld/src/main/java/ru/otus/sansanpetrov/helloworld.java
@@ -1,7 +1,49 @@
 package ru.otus.sansanpetrov;
+
 // Создаем класс helloworld
 public class helloworld {
-    public static void main(String args []) { // Создаем метод main (основной метод, тело программы)
-System.out.println("Hello, World!"); // Выводим строку в консоль
+
+    //Выполнение параграфа 1
+    public static void greeting() { //Создаем метод greeting- вывод приветствия
+        System.out.println("Paragraph 1\n");
+        System.out.println("Hello\nWorld\nfrom\nJava\n"); // Выводим приветствие в консоль
+        System.out.println("END of paragraph 1\n");
+    }
+// Выполнение параграфа 2
+    public static void checkSign(int a, int b, int c) { //Создаем метод checkSum- сравнение суммы с нулем
+        System.out.println("Paragraph 2\n");
+
+        int sum = a + b + c;   // Вычисляем сумму
+
+        //Далее сравниваем сумму с нулем и выводим результат сообщением в консоль
+        if (sum >= 0) {
+            System.out.println("Сумма положительная\n");
+        } else {
+            System.out.println("Сумма отрицательная\n");
+            System.out.println("END of paragraph 2\n");
+        }
+
+    }
+    //Выполнение параграфа 3
+    public static void selectColor() {
+        System.out.println("Paragraph 3\n");
+        int data = 21;
+        if (data <= 10) {                    // Сравниваем с 10 и выводим "Красный"
+            System.out.println("Красный\n");
+        }
+        if (data > 10 && data <= 20) {              // Убеждаемся, что значение между 10 и 20
+            System.out.println("Желтый\n");       //Выводим "Желтый"
+        }
+        if (data > 20) {
+            System.out.println("Зеленый\n");  // Выводим "Зеленый", если значение больше 20
+        }
+        System.out.println("END of paragraph 3\n");
+    }
+
+// Выполнение всех задач
+    public static void main(String args[]) { // Создаем метод main (основной метод, тело программы)
+        greeting();
+        checkSign(3, 5, -10);
+selectColor();
     }
 }
