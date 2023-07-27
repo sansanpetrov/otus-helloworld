ed178b60f375522b5f3731f6ed5f06d795b638b2
Alexander Petrov <san.san.petrov@yandex.ru>
2023-07-27 21:25:06 +0300

    Пункт 3 ДЗ № 2 выполнен.

--- HelloWorld/src/main/java/ru/otus/sansanpetrov/HomeWork2/Hw2Massive.java
+++ HelloWorld/src/main/java/ru/otus/sansanpetrov/HomeWork2/Hw2Massive.java
@@ -44,16 +44,43 @@ public class Hw2Massive {
         }
         System.out.println("Созданный массив: " + Arrays.toString(vector));
         System.out.println("Сумма элементов массива с индексом больше 5: " + sum);
+    }
 
+    // Выполняем пункт № 3
+    public static void integerEqual() {
+        int integer = 33;
+        int[] equal = new int[25];
+        for (int i = 0; i < 25; i++) {
+            equal[i] = integer;
+        }
+        System.out.println("Значения массива equal: " + Arrays.toString(equal));
     }
 
     // Запускаем основную программу со всеми методами по очереди:
     public static void main(String args[]) {
+        // Начинаем выполнение второго домашнего задания.
+        System.out.println("Домашнее задание № 2:");
 
-// Выводим метод первого параграфа
+        // Выводим метод первого параграфа
+        System.out.println("Выполнение пункта 1.");
         printString();
+        System.out.println("END of paragraph 1.");
+        System.out.println();
 
         // Выводим метод второго параграфа
+        System.out.println("Выполнение пункта 2.");
         sumArrayMore5();
+        System.out.println("END of paragraph 2.");
+        System.out.println();
+
+        // Выводим метод третьего параграфа:
+        System.out.println("Выполнение пункта 3.");
+        integerEqual();
+        System.out.println("END of paragraph 3.");
+        System.out.println();
+
+        // Выводим сообщение о завершении выполнения ДЗ № 2.
+        System.out.println("Домашнее задание № 2 закончено..");
+        System.out.println();
     }
 }
