49f4c44579458202965fac002c88450967aac1b7
Alexander Petrov <san.san.petrov@yandex.ru>
2023-07-28 14:51:05 +0300

    Начало выполнения ДЗ № 3

---
+++ HelloWorld/src/main/java/ru/otus/sansanpetrov/Hw3/HomeWork3.java
@@ -0,0 +1,13 @@
+package ru.otus.sansanpetrov.Hw3;
+
+public class HomeWork3 {
+    //Метод, реализующий первый параграф задания
+    public static void sumOfPositiveElements() {
+
+    }
+
+ public static void main() {
+        // Выводим результаты первого пункта задания
+        sumOfPositiveElements();
+    }
+}
