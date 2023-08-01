1c8f234f916186f03e21ac2c20e74ed3e56a8491
sansanpetrov <san.san.petrov@yandex.ru>
2023-08-01 10:21:46 +0300

    Пункт 1 ДЗ 3 выполнен

--- HelloWorld/src/main/java/ru/otus/sansanpetrov/Hw3/HomeWork3.java
+++ HelloWorld/src/main/java/ru/otus/sansanpetrov/Hw3/HomeWork3.java
@@ -2,13 +2,22 @@ package ru.otus.sansanpetrov.hw3;
 
 public class HomeWork3 {
     //Метод, реализующий первый параграф задания
-    public static void sumOfPositiveElements() {
-int[] arrayDouble = new int [3] [3];
-arrayDouble
-    }
-
- public static void main() {
+    public static void sumOfPositiveElements(int[][] arrayDouble) {
+ int sumOfPositive = 0;
+        for (int i = 0; i < arrayDouble.length; i++) {
+            for (int j = 0; j < arrayDouble[i].length; j++) {
+                if(arrayDouble[i][j] > 0) {
+                    sumOfPositive += arrayDouble[i][j];
+                }
+            }
+        }
+        System.out.print("Сумма элементов массива больше нуля:" + sumOfPositive);
+        System.out.println();
+ }
+    public static void main(String[] args) {
         // Выводим результаты первого пункта задания
-        sumOfPositiveElements();
+        System.out.println("Первый пункт третьего задания:");
+        sumOfPositiveElements(new int [][] {{0, 2, 55, 26},{5, 0, 0, 37}});
+        System.out.println("Конец выполнения первого пункта.");
     }
 }
