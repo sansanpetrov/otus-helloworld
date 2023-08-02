ad878e998acb6946f4f7d2550db9299f6c9737b2
sansanpetrov <san.san.petrov@yandex.ru>
2023-08-02 08:56:25 +0300

    Пункт 3 ДЗ 3 выполнен

--- HelloWorld/src/main/java/ru/otus/sansanpetrov/Hw3/HomeWork3.java
+++ HelloWorld/src/main/java/ru/otus/sansanpetrov/Hw3/HomeWork3.java
@@ -22,8 +22,8 @@ public class HomeWork3 {
                     squareStarrr[i][j] = '*';
                 }
             }
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size; j++) {
+            for (int i = 0; i < squareStarrr.length; i++) {
+                for (int j = 0; j < squareStarrr[i].length; j++) {
                     System.out.print(squareStarrr[i][j] + " ");
                 }
                 System.out.println();
@@ -31,7 +31,19 @@ public class HomeWork3 {
         }
 
         //Метод, реализующий третий параграф задания
-
+public static void zeroDiag(int[][] zero) {
+  //     int[][] zero = new int[3][3];  - Не понимаю, как давать массивы на вход метода.
+    //     Работает только через new int[][]
+    for (int i = 0; i < zero.length; i++) {
+        for (int j = 0; j < zero[i].length; j++) {
+            if(i == j) {
+                zero[i][j] = 0;
+            }
+            System.out.print(zero[i][j] + " ");
+        }
+        System.out.println();
+    }
+}
 
         //Метод, реализующий четвертый параграф задания
 
@@ -44,26 +56,31 @@ public class HomeWork3 {
         System.out.println("Первый пункт третьего задания:");
         sumOfPositiveElements(new int[][]{{0, 2, 55, 26}, {5, 0, 0, 37}});
         System.out.println("Конец выполнения первого пункта.");
+        System.out.println();
 
         // Выводим результаты второго пункта задания:
         System.out.println("Второй пункт третьего задания:");
         squareStar(8);
         System.out.println("Конец выполнения второго пункта.");
+        System.out.println();
 
         // Выводим результаты третьего пункта задания:
         System.out.println("Третий пункт третьего задания:");
-
+zeroDiag(new int[][]{{25, 25, 25}, {25, 25, 25}, {25, 25, 25}});
         System.out.println("Конец выполнения третьего пункта.");
+        System.out.println();
 
         // Выводим результаты четвертого пункта задания:
         System.out.println("Четвертый пункт третьего задания:");
 
         System.out.println("Конец выполнения четвертого пункта.");
+        System.out.println();
 
         // Выводим результаты пятого пункта задания:
         System.out.println("Пятый пункт третьего задания:");
 
         System.out.println("Конец выполнения пятого пункта.");
+        System.out.println();
 
         System.out.println("Конец выполнения третьего домашнего задания.");
     }
