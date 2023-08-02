7de683f118fde0ef6c6fc50d4da203a710a5d67a
sansanpetrov <san.san.petrov@yandex.ru>
2023-08-02 15:57:39 +0300

    Пункт 5 ДЗ 3 выполнен. Задание выполнено полностью.

--- HelloWorld/src/main/java/ru/otus/sansanpetrov/Hw3/HomeWork3.java
+++ HelloWorld/src/main/java/ru/otus/sansanpetrov/Hw3/HomeWork3.java
@@ -47,27 +47,36 @@ public class HomeWork3 {
     }
 
     //Метод, реализующий четвертый параграф задания
-    public static void findMax() {
-        int[][] big = new int[6][6];
+    public static void findMax(int[][] big) {
+        //      int[][] big = new int[6][6];
         int a = 0;
         for (int i = 0; i < big.length; i++) {
             for (int j = 0; j < big[i].length; j++) {
-                big[i][j] = (int) (Math.random() * 100);
-                System.out.print(big[i][j] + " ");
+                //              big[i][j] = (int) (Math.random() * 100); - Жалко удалять!
+                //              System.out.print(big[i][j] + " ");  - Жалко удалять!
                 if (big[i][j] >= a) {
                     a = big[i][j];
                 }
             }
-            System.out.println();
+            //           System.out.println();  - Жалко удалять!
         }
         System.out.println("Наибольшее число:" + a);
-           System.out.println();
-        }
-
-
-
+        System.out.println();
+    }
 
     //Метод, реализующий пятый параграф задания
+    public static void sumSecondString(int[][] sSS) {
+        int a = 0;
+        if (sSS[1].length >= 1) {
+            for (int j = 0; j < sSS[1].length; j++) {
+                a += sSS[1][j];
+            }
+        } else {
+            a = -1;
+        }
+        System.out.println("Сумма элементов второй строки массива:" + a);
+        System.out.println();
+    }
 
 
     public static void main(String[] args) {
@@ -91,13 +100,13 @@ public class HomeWork3 {
 
         // Выводим результаты четвертого пункта задания:
         System.out.println("Четвертый пункт третьего задания:");
-        findMax();
+        findMax(new int[][]{{26, 67, 128, 327}, {129, 998, 786, 1029}, {3289, 8888, 765, 2}, {238, 877, 887, 876}});
         System.out.println("Конец выполнения четвертого пункта.");
         System.out.println();
 
         // Выводим результаты пятого пункта задания:
         System.out.println("Пятый пункт третьего задания:");
-
+        sumSecondString(new int[][]{{26, 67, 128, 327}, {129, 998, 786, 1029}, {3289, 8888, 765, 2}, {238, 877, 887, 876}});
         System.out.println("Конец выполнения пятого пункта.");
         System.out.println();
 
