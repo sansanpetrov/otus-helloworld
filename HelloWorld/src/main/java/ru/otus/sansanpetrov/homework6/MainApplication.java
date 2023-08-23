decf98e42e24616aba58dd6cd9da634b3c96af4b
sansanpetrov <san.san.petrov@yandex.ru>
2023-08-23 19:17:15 +0300

    ДЗ № 6 выполнено.

--- HelloWorld/src/main/java/ru/otus/sansanpetrov/homework6/MainApplication.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.sansanpetrov.homework6;
-
-public class MainApplication {
-    public static void main(String[] args) {
-
-    }
-}
--- HelloWorld/src/main/java/ru/otus/sansanpetrov/homework6/Plate.java
+++ HelloWorld/src/main/java/ru/otus/sansanpetrov/homework6/Plate.java
@@ -2,21 +2,42 @@ package ru.otus.sansanpetrov.homework6;
 
 public class Plate {
     // Поля
-    private String name;
+    private int foodVolumeMax; // Максимальная вместимость тарелки едой
+    private int foodVolumeCurrent; // Текущая наполненность тарелки едой
 
 
     // Конструктор
-    public Plate(String name) {
-        this.name = name;
+    public Plate(int foodVolumeMax, int foodVolumeCurrent) {
+        this.foodVolumeMax = foodVolumeMax;
+        this.foodVolumeCurrent = foodVolumeCurrent;
     }
 
     // getters / setters
 
-    public String getName() {
-        return name;
+    public int getFoodVolumeMax() {
+        return foodVolumeMax;
     }
 
-    // Методы класса
+    public int getFoodVolumeCurrent() {
+        return foodVolumeCurrent;
+    }
 
+    // Когда кот ест и уменьшает количество еды, метод решает, хватило ли ему еды в тарелке.
+    public boolean decreaseFood(int amount) {
+        foodVolumeCurrent -= amount;
+        if (foodVolumeCurrent < 0) {
+            this.foodVolumeCurrent = 0;  // Эта строка нужна для применения метода добавки еды в тарелку
+            return false;
+        } else {
+            return true;
+        }
+    }
 
+    // Увеличиваем количество еды в тарелке
+    public void increaseFood(int amount) {
+        foodVolumeCurrent += amount;
+        if (foodVolumeCurrent >= foodVolumeMax) {
+            this.foodVolumeMax = 100;
+        }
+    }
 }
