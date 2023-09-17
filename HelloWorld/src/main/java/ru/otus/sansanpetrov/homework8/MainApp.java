f5b3f9bfd6dee39c77a12e2ea982e699ee42ec6e
sansanpetrov <san.san.petrov@yandex.ru>
2023-09-17 10:45:34 +0300

    ДЗ № 8: правки сделаны. Добавлено форматирование.

--- HelloWorld/src/main/java/ru/otus/sansanpetrov/homework8/MainApp.java
+++ HelloWorld/src/main/java/ru/otus/sansanpetrov/homework8/MainApp.java
@@ -6,7 +6,7 @@ public class MainApp {
     public static void massive4Size(String[][] sentences) {
         int sum = 0;
         for (int i = 0; i < sentences.length; i++) {
-            for (int j = 0; j <sentences[i].length; j++) {
+            for (int j = 0; j < sentences[i].length; j++) {
                 if (sentences.length != 4 || sentences[i].length != 4) {
                     System.out.println("Длина текущего массива " + sentences.length);
                     throw new AppArraySizeException("Длина массива не равна 4!\n");
@@ -28,10 +28,10 @@ public class MainApp {
 
     public static void main(String[] args) {
         try {
-             //     massive4Size(new String[][]{{"1", "2", "3", "4", "9"}, {"5", "6", "7", "8", "10"}});
+            //     massive4Size(new String[][]{{"1", "2", "3", "4", "9"}, {"5", "6", "7", "8", "10"}});
             massive4Size(new String[][]{{"1", "2", "3", "4", "20"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}});
             //       massive4Size(new String[][]{{"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}});
-      //      massive4Size(new String[][]{{"1", "Cat", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}});
+            //      massive4Size(new String[][]{{"1", "Cat", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}});
         } catch (AppArraySizeException e) {
             e.printStackTrace();
         } catch (AppArrayDataException e) {
