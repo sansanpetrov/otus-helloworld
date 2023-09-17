c0d407a11f9d13a94d0dc72cb36abb0e2f0479b3
sansanpetrov <san.san.petrov@yandex.ru>
2023-09-17 10:44:05 +0300

    ДЗ № 8: правки сделаны.

--- HelloWorld/src/main/java/ru/otus/sansanpetrov/homework8/MainApp.java
+++ HelloWorld/src/main/java/ru/otus/sansanpetrov/homework8/MainApp.java
@@ -1,34 +1,41 @@
 package ru.otus.sansanpetrov.homework8;
+
 public class MainApp {
 
     // Метод, принимающий массив 4х4. Выполняем пункт № 1
-     public static void massive4Size(String[][] sentences) throws AppArraySizeException, AppArrayDataException, NumberFormatException {
-         int sum = 0;
-         if (sentences.length != 4) {
-             System.out.println("Длина текущего массива " + sentences.length);
-             throw new AppArraySizeException("Длина массива не равна 4!\n");
-         }
-             for (int i = 0; i < sentences.length; i++) {
-                 for (int j = 0; j < sentences[i].length; j++) {
-                  try {
-                      Integer.parseInt(sentences[i][j]);
-                  } catch  (NumberFormatException e) {
-                     throw new AppArrayDataException("Ячейка (" + i + "," + j + ") содержит слово");
-                     }
-                     sum += Integer.parseInt(sentences[i][j]);
-                 }
-             }
-             System.out.println("Сумма всех преобразованных элементов массива= " + sum);
-         }
-         public static void main (String[] args){
-             try {
-           //      massive4Size(new String[][]{{"1", "2", "3", "4", "9"}, {"5", "6", "7", "8", "10"}});
-          //       massive4Size(new String[][]{{"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}});
-                 massive4Size(new String[][]{{"1", "Cat", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}});
-             } catch (AppArraySizeException e) {
-                 e.printStackTrace();
-             } catch (AppArrayDataException e) {
-                 e.printStackTrace();
-             }
-         }
+    public static void massive4Size(String[][] sentences) {
+        int sum = 0;
+        for (int i = 0; i < sentences.length; i++) {
+            for (int j = 0; j <sentences[i].length; j++) {
+                if (sentences.length != 4 || sentences[i].length != 4) {
+                    System.out.println("Длина текущего массива " + sentences.length);
+                    throw new AppArraySizeException("Длина массива не равна 4!\n");
+                }
+            }
+        }
+        for (int i = 0; i < sentences.length; i++) {
+            for (int j = 0; j < sentences[i].length; j++) {
+                try {
+                    Integer.parseInt(sentences[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new AppArrayDataException("Ячейка (" + i + "," + j + ") содержит слово");
+                }
+                sum += Integer.parseInt(sentences[i][j]);
+            }
+        }
+        System.out.println("Сумма всех преобразованных элементов массива= " + sum);
+    }
+
+    public static void main(String[] args) {
+        try {
+             //     massive4Size(new String[][]{{"1", "2", "3", "4", "9"}, {"5", "6", "7", "8", "10"}});
+            massive4Size(new String[][]{{"1", "2", "3", "4", "20"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}});
+            //       massive4Size(new String[][]{{"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}});
+      //      massive4Size(new String[][]{{"1", "Cat", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}});
+        } catch (AppArraySizeException e) {
+            e.printStackTrace();
+        } catch (AppArrayDataException e) {
+            e.printStackTrace();
+        }
+    }
 }
