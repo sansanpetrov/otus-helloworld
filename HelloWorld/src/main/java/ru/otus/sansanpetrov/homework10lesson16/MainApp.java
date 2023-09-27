c50297f3b1badac01579d5b2484693523bd00abd
sansanpetrov <san.san.petrov@yandex.ru>
2023-09-27 07:41:19 +0300

    ДЗ № 10: начало.

---
+++ HelloWorld/src/main/java/ru/otus/sansanpetrov/homework10lesson16/MainApp.java
@@ -0,0 +1,12 @@
+package ru.otus.sansanpetrov.homework10lesson16;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainApp {
+    public static void main(String[] args) {
+        String mane;
+        HashMap<name, ArrayList> book = new HashMap<name, ArrayList>();
+        System.out.println(book);
+    }
+}
---
+++ HelloWorld/src/main/java/ru/otus/sansanpetrov/homework10lesson16/PhoneBook.java
@@ -0,0 +1,42 @@
+package ru.otus.sansanpetrov.homework10lesson16;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class PhoneBook {
+    // Поля
+    private String fullName;
+    private String phoneNumber;
+
+    // Конструктор
+
+    public PhoneBook(String fullName, String phoneNumber) {
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+    }
+    // Getters / setters
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    // Main methods
+// Добавляем запись имя- телефоны
+    public void add(HashMap<PhoneBook> cellulars, String fullName, String phoneNumber) {
+ HashMap<String, ArrayList<>>
+    }
+}
