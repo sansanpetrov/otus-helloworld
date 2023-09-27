2429e5a03984ebf4c93f1d5ee14bb47d8da8160a
sansanpetrov <san.san.petrov@yandex.ru>
2023-09-27 10:18:11 +0300

    ДЗ № 17: переделка скрипта.

--- HelloWorld/src/main/java/ru/otus/sansanpetrov/homework17lesson24/tests.sql
+++ HelloWorld/src/main/java/ru/otus/sansanpetrov/homework17lesson24/tests.sql
@@ -4,37 +4,48 @@
 
 CREATE SCHEMA IF NOT EXISTS tests
     AUTHORIZATION postgres;
-	
--- CREATE TABLES ;	
-	CREATE TABLE IF NOT EXISTS tests.testcase (
-	id smallserial NOT NULL,
-		testname VARCHAR(256) NOT NULL,
-		preconditions VARCHAR(256),
-		functionalstep VARCHAR(256),
+
+-- CREATE TABLES ;
+
+-- Table for testcases ;
+
+	CREATE TABLE IF NOT EXISTS tests.testcases (
+	id serial4 NOT NULL,
+		testcasename VARCHAR(255) NOT NULL,
+		preconditions VARCHAR(255),
 		obsolete boolean DEFAULT FALSE,
-		CONSTRAINT testcase_pk PRIMARY KEY (id)
+		CONSTRAINT testcases_pk PRIMARY KEY (id),
+		CONSTRAINT testcases_un UNIQUE (testcasename)
 	);
-	
-	CREATE TABLE IF NOT EXISTS tests.verificationstep (
-	id smallserial NOT NULL,
-		testcase_functionalstep VARCHAR(256),
-		verificationstep1 boolean,
-		verificationstep2 boolean,
-		verificationstep3 boolean,
-		verificationstep4 boolean,
-		verificationstep5 boolean,
-		CONSTRAINT verificationstep_pk PRIMARY KEY (id)
+
+-- Table for Test steps and expected results ;
+
+	CREATE TABLE IF NOT EXISTS tests.teststeps (
+	id serial4 NOT NULL,
+		functionalstep VARCHAR(255),
+		expectedresult VARCHAR(255),
+		stepresult boolean,
+		testcase_id serial4 NOT NULL,
+		CONSTRAINT teststeps_pk PRIMARY KEY (id),
+		CONSTRAINT teststeps_fk FOREIGN KEY (testcase_id) REFERENCES tests.testcases(id)
 	);
-	
-	CREATE TABLE IF NOT EXISTS tests.checklist (
-		id smallserial NOT NULL,
-		listname VARCHAR(256) NOT NULL,
-		testcase_id smallserial NOT NULL,
-		CONSTRAINT checklist_pk PRIMARY KEY (id)
+
+-- Table for checklists	;
+
+	CREATE TABLE IF NOT EXISTS tests.checklists (
+		id serial4 NOT NULL,
+		checklistname VARCHAR(255) NOT NULL,
+		testcase_id serial4 NOT NULL,
+		CONSTRAINT checklists_pk PRIMARY KEY (id),
+		CONSTRAINT checklists_fk FOREIGN KEY (testcase_id) REFERENCES tests.testcases(id)
 	);
-	CREATE TABLE IF NOT EXISTS tests.results (
-		id smallserial NOT NULL,
-		checklist_id smallserial NOT NULL,
+
+-- Table for
+
+	CREATE TABLE IF NOT EXISTS tests.checklistresults (
+		id serial4 NOT NULL,
+		checklist_id serial4 NOT NULL,
 		testresults boolean,
-		CONSTRAINT results_pk PRIMARY KEY (id)
+		CONSTRAINT checklistresults_pk PRIMARY KEY (id),
+		CONSTRAINT checklistresults_fk FOREIGN KEY (checklist_id) REFERENCES tests.checklists(id)
 	);
