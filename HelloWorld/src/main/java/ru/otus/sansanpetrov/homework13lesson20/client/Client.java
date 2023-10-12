72dc81b4a826796a9ec8a929106935b7b7955797
sansanpetrov <san.san.petrov@yandex.ru>
2023-10-12 09:28:26 +0300

    ДЗ № 13- попытки выявить причины ошибки NullPointerException

--- HelloWorld/src/main/java/ru/otus/sansanpetrov/homework13lesson20/client/Client.java
+++ HelloWorld/src/main/java/ru/otus/sansanpetrov/homework13lesson20/client/Client.java
@@ -1,4 +1,47 @@
 package ru.otus.sansanpetrov.homework13lesson20.client;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+
+    public static void main(String[] args) throws RuntimeException, IOException {
+        try (Socket socket = new Socket("localhost", 8088)) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            // Получили сообщение от сервера про возможные операции:
+            String str = in.readLine().trim();
+            System.out.println(str);
+            // Получаем приглашение ввести первое число и отправляем его:
+            String str1 = in.readLine().trim();
+            System.out.println(str1);
+            Scanner scanner1 = new Scanner(System.in);
+            Float string1 = scanner1.nextFloat();
+            System.out.println(string1);
+            out.write(string1.toString() + "\n");
+//            // Получаем приглашение ввести второе число и отправляем его:
+//            String str2 = in.readLine().trim();
+//            System.out.println(str2);
+//            Scanner scanner2 = new Scanner(System.in);
+//            Float string2 = scanner2.nextFloat();
+//            out.write(string2.toString());
+//            // Получаем приглашение ввести знак операции и отправляем его:
+//            String str3 = in.readLine().trim();
+//            System.out.println(str3);
+//            Scanner scanner3 = new Scanner(System.in);
+//            String string3 = scanner3.nextLine();
+//            out.write(string3);
+//            // Получаем ответ:
+//            String str4 = in.readLine().trim();
+//            System.out.println(str4);
+        }
+    }
 }
+
+//    @Override
+//    public void close() throws Exception {
+//        in.close();
+//        out.close();
+//    }
+
--- HelloWorld/src/main/java/ru/otus/sansanpetrov/homework13lesson20/server/Server.java
+++ HelloWorld/src/main/java/ru/otus/sansanpetrov/homework13lesson20/server/Server.java
@@ -6,11 +6,12 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8088);
-        while (true) {
-            Socket client = serverSocket.accept();
-            System.out.println("Client connected.");
-            mathOperation(client);
+        try (ServerSocket serverSocket = new ServerSocket(8088)) {
+            while (true) {
+                Socket client = serverSocket.accept();
+                System.out.println("Client connected.");
+                mathOperation(client);
+            }
         }
     }
 
@@ -19,47 +20,47 @@ public class Server {
         String number;
         BufferedReader listener = new BufferedReader(new InputStreamReader(client.getInputStream()));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-        System.out.println("Сетевой калькулятор может вычислять следующие математические операции: +,-,*,/");
-        out.write("Сетевой калькулятор может вычислять следующие математические операции: +,-,*,/");
-        while (true) {
-            System.out.println("Введите первое число: ");
-            out.write("Введите первое число: ");
-            number = listener.readLine().trim();
-            System.out.println(number);
-            Float number1 = Float.valueOf(number);
+        out.write("Сетевой калькулятор может вычислять следующие математические операции: +,-,*,/" + "\n");
+        out.flush();
+//        while (true) {
+        out.write("Введите первое число: " + "\n");
+        out.flush();
+//        number = listener.readLine().trim();
+         number = listener.readLine();
+        System.out.println(number);
+//        Float number1 = Float.valueOf(number);
 
-            System.out.println("Введите второе число: ");
-            out.write("Введите второе число: ");
-            number = listener.readLine().trim();
-            System.out.println(number);
-            Float number2 = Float.valueOf(number);
-
-
-            System.out.println("Введите знак операции: ");
-            String operation = listener.readLine();
-            System.out.println(operation);
-            while (operation == null || operation.trim().isEmpty()) {
-                System.out.println("Пожалуйста введите знак операции ");
-                break;
-            }
-            if (operation.equals("+")) {
-                result = number1 + number2;
-                out.write(result.toString());
-            }
-            if (operation.equals("-")) {
-                result = number1 - number2;
-                out.write(result.toString());
-            }
-            if (operation.equals("*")) {
-                result = number1 * number2;
-                out.write(result.toString());
-            }
-            if (operation.equals("/")) {
-                result = number1 / number2;
-                out.write(result.toString());
-            } else {
-                System.out.println("Неправильный знак операции");
-            }
-        }
+//            out.write("Введите второе число: " + "\n");
+//            out.flush();
+//            number = listener.readLine().trim();
+//            System.out.println(number);
+//            Float number2 = Float.valueOf(number);
+//
+//            String operation = listener.readLine();
+//            System.out.println(operation);
+//            while (operation == null || operation.trim().isEmpty()) {
+//                out.write("Пожалуйста введите знак операции " + "\n");
+//                out.flush();
+//                break;
+//            }
+//            if (operation.equals("+")) {
+//                result = number1 + number2;
+//                out.write(result.toString());
+//            }
+//            if (operation.equals("-")) {
+//                result = number1 - number2;
+//                out.write(result.toString());
+//            }
+//            if (operation.equals("*")) {
+//                result = number1 * number2;
+//                out.write(result.toString());
+//            }
+//            if (operation.equals("/")) {
+//                result = number1 / number2;
+//                out.write(result.toString());
+//            } else {
+//                System.out.println("Неправильный знак операции" + "\n");
+//            }
+        //       }
     }
 }
